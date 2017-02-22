1ec4e1bc2632de17da5ca95fdbe3951d506c83a1
iago.cguimaraes <iago.cguimaraes@CAS511761W0733.senacsp.edu.br>
2017-02-21 21:25:04 -0300

    printar se a variavel i for par

--- aula01/src/main/java/br/senac/tads/pi3/aula01/aula01/Main.java
+++ aula01/src/main/java/br/senac/tads/pi3/aula01/aula01/Main.java
@@ -1,10 +1,12 @@
-
 package br.senac.tads.pi3.aula01.aula01;
 
 public class Main {
+
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
-           System.out.println("OK!"); 
-        }      
+            if (i % 2 == 0) {
+                System.out.println(i + " OK!");
+            }
+        }
     }
 }
